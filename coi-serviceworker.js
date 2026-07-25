cc890f4796b1cd05dc823d16314068cb1176e55a
Artus030 <m.gessler2004@gmail.com>
2026-07-25 11:33:27 +0200

    fixes

---
+++ coi-serviceworker.js
@@ -0,0 +1,23 @@
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window !== 'undefined') {
+    coepCredentialless = window.crossOriginEmbedderPolicy === 'credentialless';
+}
+
+const n = navigator;
+if (n.serviceWorker) {
+    n.serviceWorker.register(document.currentScript.src).then(
+        (registration) => {
+            console.log('COOP/COEP Service Worker registered', registration.scope);
+            registration.addEventListener('updatefound', () => {
+                window.location.reload();
+            });
+            if (registration.active && !n.serviceWorker.controller) {
+                window.location.reload();
+            }
+        },
+        (err) => {
+            console.error('COOP/COEP Service Worker failed: ', err);
+        }
+    );
+}
