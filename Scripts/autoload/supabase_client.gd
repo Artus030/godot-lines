7a44d5bc27376b53af213e96e0d9c49d4c73a96f
Artus030 <m.gessler2004@gmail.com>
2026-07-31 16:08:21 +0200

    fixes

--- Scripts/autoload/supabase_client.gd
+++ Scripts/autoload/supabase_client.gd
@@ -33,22 +33,57 @@ func request(endpoint: String, method: HTTPClient.Method, data: Dictionary, call
 
 func _request_web(endpoint: String, method: HTTPClient.Method, data: Dictionary, callback: Callable) -> void:
 	var cb_id = "godot_cb_" + str(Time.get_ticks_usec())
+	var method_str = "GET" if method == HTTPClient.METHOD_GET else "POST"
 	
-	# Callback erstellen & aufräumen
+	# Callback für Godot
 	var js_cb = JavaScriptBridge.create_callback(func(args):
 		JavaScriptBridge.get_interface("window").delete_property(cb_id)
 		if callback.is_valid():
-			callback.call(int(args[0]), JSON.parse_string(String(args[1])))
+			var status_code = int(args[0])
+			var raw_body = String(args[1])
+			
+			# Debug-Print für die Browser-Konsole (F12)
+			print("WEB RESPONSE -> Code: ", status_code, " | Body: ", raw_body)
+			
+			var parsed_json = JSON.parse_string(raw_body)
+			callback.call(status_code, parsed_json)
 	)
+	
 	JavaScriptBridge.get_interface("window")[cb_id] = js_cb
 
-	# Kompakter JS-Fetch
-	var body_js = ", body: '%s'" % JSON.stringify(data) if not data.is_empty() and method != HTTPClient.METHOD_GET else ""
+	# Body NUR bei POST/PUT anhängen
+	var body_option = ""
+	if not data.is_empty() and method != HTTPClient.METHOD_GET:
+		body_option = "body: JSON.stringify(%s)," % JSON.stringify(data)
+
 	var js_code = """
-	fetch('%s', { method: '%s', headers: {'apikey': '%s', 'Authorization': 'Bearer %s', 'Content-Type': 'application/json'} %s })
-		.then(r => r.text().then(t => window['%s'](r.status, t)))
-		.catch(e => window['%s'](0, ""));
-	""" % [SUPABASE_URL + endpoint, "GET" if method == HTTPClient.METHOD_GET else "POST", PUBLISHABLE_KEY, PUBLISHABLE_KEY, body_js, cb_id, cb_id]
+	(async function() {
+		try {
+			let response = await fetch('%s', {
+				method: '%s',
+				headers: {
+					'apikey': '%s',
+					'Authorization': 'Bearer %s',
+					'Content-Type': 'application/json'
+				},
+				%s
+			});
+			let text = await response.text();
+			window['%s'](response.status, text);
+		} catch(e) {
+			console.error("Fetch Error:", e);
+			window['%s'](0, "");
+		}
+	})();
+	""" % [
+		SUPABASE_URL + endpoint,
+		method_str,
+		PUBLISHABLE_KEY,
+		PUBLISHABLE_KEY,
+		body_option,
+		cb_id,
+		cb_id
+	]
 
 	JavaScriptBridge.eval(js_code)
 
